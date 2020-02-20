9ff37d6f7768624e6b0cba7520cc4b461fb9df0d
comcon1 <comcon1@protonmail.com>
2020-02-20 15:42:54 +0300

    add scanner for complex model

---
+++ complex-model/scanner/launcher_1m.py
@@ -0,0 +1,258 @@
+#!/usr/bin/env python3
+
+from multiprocessing.pool import ThreadPool
+import os, time, subprocess, sys
+import numpy as np
+from numpy import pi
+import os.path
+
+of = False
+
+def calculate_error(_params, _erads, _eres, log=None):
+    '''
+    _params -- vector of results
+      0: na in control sample (Noggin2 production)
+      1: nx in all samples (Noggin2 degradation in absence of MMP3)
+      2: mmp3 amount in control sample
+      3: sa in all samples (SMAD cascade intensity)
+      4: sb in all samples (BMP amount for SMAD switch)
+      5: smad_threshold determines bifurcation point for SMAD amount (can be
+        correlated with *sa*)
+      6: nu determines rate of delta reaction to other agents
+      7: h determines the strength of delta hysteresis
+      8: tb determines the strength of chordin degradation inhibition by mmp3.
+        Less tb - more inhibition.
+      9: ca rate of Chordin in all samples
+      10: cx degratation rate of Chordin in all samples (in absence of MMP3)
+      11: s0 for definition of de_init parameter
+      12: cth determines value of SMAD activation, which defines the edge of
+        CACT expression area
+    _erads -- vector of equisurf. radii
+    _eres -- vector of results
+    @RETURN: float
+    '''
+
+    T = 10
+
+    pna = _params[0];  pnx = _params[1];  pm3 = _params[2];  psa = _params[3]
+    psb = _params[4];  pst = _params[5];  pnu = _params[6];  ph  = _params[7]
+    ptb = _params[8];  pca = _params[9];  pcx = _params[10]; s0 = _params[11]
+    pct = _params[12]; cfa = _params[13]; cfx = _params[14]; cft = _params[15] 
+
+    '''
+             T     L      na      nx   mmp3      sa   sb  smthr  nu    h   tb    ca     cx   dinit
+             0   1        2       3     4        5    6     7     8    9   10    11     12    13 
+wh.      0   T   pi*r    pna     pnx   pm3      psa  psb   pst   pnu   ph  ptb   pca    pcx   5*s0/r^2/pi
+hf.      1   T   pi*r    pna     pnx   pm3/10.  psa  psb   pst   pnu   ph  ptb   pca    pcx   5*s0/r^2/pi
+wh. -N   2   T   pi*r    pna/5.  pnx   pm3      psa  psb   pst   pnu   ph  ptb   pca    pcx   5*s0/r^2/pi
+hf. -N   3   T   pi*r    pna/5.  pnx   pm3/10.  psa  psb   pst   pnu   ph  ptb   pca    pcx   5*s0/r^2/pi
+wh.      4   T   pi*r    pna     pnx   pm3      psa  psb   pst   pnu   ph  ptb   pca    pcx   5*s0/r^2/pi
+wh. -M   5   T   pi*r    pna     pnx   pm3/5.   psa  psb   pst   pnu   ph  ptb   pca    pcx   5*s0/r^2/pi
+wh. -M-N 6   T   pi*r    pna/5.  pnx   pm3/5.   psa  psb   pst   pnu   ph  ptb   pca    pcx   5*s0/r^2/pi
+wh.      7   T   pi*r    pna     pnx   pm3      psa  psb   pst   pnu   ph  ptb   pca    pcx   5*s0/r^2/pi
+wh. -C   8   T   pi*r    pna     pnx   pm3      psa  psb   pst   pnu   ph  ptb   pca/5. pcx   5*s0/r^2/pi
+wh. -C-N 9   T   pi*r    pna/5.  pnx   pm3      psa  psb   pst   pnu   ph  ptb   pca/5. pcx   5*s0/r^2/pi
+
+ PCT is not forwarded to C++ code because it acts at the stage of result interpretation
+    '''
+    _r = _erads
+    param_array = np.array([ \
+        [   T, pi*_r[0] ,pna   , pnx , pm3    , psa, psb,  pst,  pnu,  ph, ptb,  pca   ,  pcx, 1.59e-5*s0*_r[0]**2, cfa, cfx, cft ],\
+        [   T, pi*_r[1] ,pna   , pnx , pm3/10., psa, psb,  pst,  pnu,  ph, ptb,  pca   ,  pcx, 1.59e-5*s0*_r[1]**2, cfa, cfx, cft ],\
+        [   T, pi*_r[2] ,pna/5., pnx , pm3    , psa, psb,  pst,  pnu,  ph, ptb,  pca   ,  pcx, 1.59e-5*s0*_r[2]**2, cfa, cfx, cft ],\
+        [   T, pi*_r[3] ,pna/5., pnx , pm3/10., psa, psb,  pst,  pnu,  ph, ptb,  pca   ,  pcx, 1.59e-5*s0*_r[3]**2, cfa, cfx, cft ],\
+        [   T, pi*_r[4] ,pna   , pnx , pm3    , psa, psb,  pst,  pnu,  ph, ptb,  pca   ,  pcx, 1.59e-5*s0*_r[4]**2, cfa, cfx, cft ],\
+        [   T, pi*_r[5] ,pna   , pnx , pm3/5. , psa, psb,  pst,  pnu,  ph, ptb,  pca   ,  pcx, 1.59e-5*s0*_r[5]**2, cfa, cfx, cft ],\
+        [   T, pi*_r[6] ,pna/5., pnx , pm3/5. , psa, psb,  pst,  pnu,  ph, ptb,  pca   ,  pcx, 1.59e-5*s0*_r[6]**2, cfa, cfx, cft ],\
+        [   T, pi*_r[7] ,pna   , pnx , pm3    , psa, psb,  pst,  pnu,  ph, ptb,  pca   ,  pcx, 1.59e-5*s0*_r[7]**2, cfa, cfx, cft ],\
+        [   T, pi*_r[8] ,pna   , pnx , pm3    , psa, psb,  pst,  pnu,  ph, ptb,  pca/5.,  pcx, 1.59e-5*s0*_r[8]**2, cfa, cfx, cft ],\
+        [   T, pi*_r[9] ,pna/5., pnx , pm3    , psa, psb,  pst,  pnu,  ph, ptb,  pca/5.,  pcx, 1.59e-5*s0*_r[9]**2, cfa, cfx, cft ],\
+            ])
+    nexp = param_array.shape[0]
+    assert(nexp == _eres.shape[1])
+    log.write('Parameters vector: %s\n' % (' '.join(map(str, _params)))  )
+    log.write('Radii vector: %s\n' % (' '.join(map(str, _erads)))  )
+    # param_strings - parameters ready for getting to apply_async 
+    param_strings = [[] for x in range(nexp)]
+    for i in range(nexp):
+        param_strings[i] += [ "-O" ]
+        param_strings[i] += [ "_current_/exp%d.txt" % (i) ]
+        param_strings[i] += [ "--input-params" ]
+        param_strings[i] += map(str, param_array[i,:])
+    # check _current_ folder
+    #print(param_strings)
+    try:
+        _fd = open('_current_/1', 'w')
+        _fd.write('check')
+        _fd.close()
+    except IOError as e:
+        print("Cant write files into _current_")
+        print(e)
+        sys.exit(1)
+
+    try:
+        pass
+        perform_simulations(param_strings, log)
+    except Exception as e:
+        print("Error in performing simulations. Killing..")
+        print(e)
+        sys.exit(1)
+
+    try:
+        g_results = extract_results_delta(param_array[:,1], "_current_", log)
+        s_results = extract_results_smad(param_array[:,1], "_current_", log, \
+                psa, psb, pct)
+    except Exception as e:
+        print("Error in extracting results. Killing..")
+        raise e
+
+    # sum CHD ignoring None
+    rSQ = 0; _n = 0
+    for ires in range(nexp):
+        if not (_eres[0,ires] is None):
+            rSQ += (_eres[0,ires] - g_results[ires])**2
+            _n += 1
+    
+    rVal = 3*rSQ/_n
+
+    # sum CACT ignoring None
+    rSQ = 0; _n = 0;
+    for ires in range(nexp):
+        if not (_eres[1,ires] is None):
+            rSQ += (_eres[1,ires] - s_results[ires])**2
+            _n += 1
+    
+    rVal += rSQ/_n
+    print(' RESULT DELTA VEC: %s\n' % (' '.join(map(str, g_results)) ))
+    print(' RESULT SMAD VEC: %s\n' % (' '.join(map(str, s_results)) ))
+    log.write('Resulting Delta: %s\n' % \
+            (' '.join(map(lambda x: '%5.1f' % x, g_results)))  )
+    log.write('Resulting  Smad: %s\n' % \
+            (' '.join(map(lambda x: '%5.1f' % x, s_results)))  )
+    log.write('Summary result: %f\n' % (rVal))
+    log.flush()
+
+    return rVal
+
+# performing single simulation series
+# parralelizing it into ncores
+# paramSL - list of strings
+# log - opened descriptor for log writing
+def perform_simulations(paramSL, log):
+    program_file = '../solver/tw.x'  
+    try:
+        num_cores = int(os.environ['L1M_NTHREADS'])
+    except:
+        num_cores = 2
+    print('Use %d number of cores' % (num_cores) )
+    nexp = len(paramSL)
+
+    tp = ThreadPool(num_cores)
+    for i in range(nexp):
+        # launch(program_file, params, repeats)
+        # launch(program_file, i, params_list[i,:] )
+        tp.apply_async(launch, ( program_file, paramSL[i] ) )
+    tp.close()
+    tp.join()
+    pass
+
+
+# Extracting the main result from the trajectories
+# Ls - array of simulation lengths, L (mcm)
+# dirname - folder with trajectory files
+# log - log descriptor
+def extract_results_delta(Ls, dirname, log):
+    nexp = len(Ls)
+    res = np.zeros(nexp)
+    try:
+        for i in range(nexp):
+            a = np.loadtxt(os.path.join(dirname, 'exp%d.txt' % (i)))
+            Nx = (a.shape[1]-1) / 5.
+            assert(abs(Nx - int(Nx)) < 1e-8) 
+            Nx = int(Nx)
+            print('Matrix loaded: ', Nx)
+            xx = np.linspace(0, Ls[i], Nx)
+            de = a[:,1+3*Nx:1+4*Nx] # delta distribution
+            grd = np.gradient(de, axis=1)
+            if (grd[-1,:].max() < 0.01):
+                print('Maximum gradient: ', grd[-1,:].max())
+                res[i] = -666
+                continue
+            delta_borders = xx[np.argmax(grd, axis=1)]
+            res[i] = delta_borders[-1]
+    except IOError as e:
+        print('File does not exist!')
+        raise e
+
+    return res
+
+def extract_results_smad(Ls, dirname, log, s_a, s_b, ctsh):
+    nexp = len(Ls)
+    res = np.zeros(nexp)
+    try:
+        for i in range(nexp):
+            a = np.loadtxt(os.path.join(dirname, 'exp%d.txt' % (i)))
+            Nx = (a.shape[1]-1) / 5.
+            assert(abs(Nx - int(Nx)) < 1e-8) 
+            Nx = int(Nx)
+            print('Matrix loaded: ', Nx)
+            xx = np.linspace(0, Ls[i], Nx)
+            bm = a[:,1+4*Nx:] # BMP distribution
+            sm = s_a*(bm/s_b)**2/(1+ (bm/s_b)**2) # SMAD distribution
+            cact_border_i = sm[-1,:].searchsorted(ctsh)
+            res[i] = Ls[i] if cact_border_i == Nx else xx[cact_border_i]
+    except IOError as e:
+        print('File does not exist!')
+        raise e
+
+    return res
+
+
+# Launching the process
+# * progname - the full path to exec file
+# * params - list of ALL the parameters
+def launch(progname, params):
+    start_time = time.time()
+    print('Started..')
+    lst = [ progname ] +\
+      list(map(str,params))
+    print(' '.join(lst))
+    out = subprocess.Popen(lst, universal_newlines=True, stdout=subprocess.PIPE)
+    z = out.wait()
+    print('Return: %d' % z)
+    # while True:
+    #   row = out.stdout.readline()
+    #   if row == '':
+    #     break
+    #   print(row,end='')
+    print('Ended: %f' % (time.time() - start_time) )
+    return
+
+
+# main body starter
+if __name__ == '__main__':
+
+    tsV = np.array([
+        [ 256., 327., 288., 214., 239., 386., 309., 296., 358., 361. ],
+        [ None, None, None, None,1374., 989., None, None, None, None ]
+                    ])
+
+    rsE = np.array(
+        [ 725., 551., 698., 538., 670., 649., 637., 791., 785., 770. ])
+
+    # =======================
+    #    pna  pnx  pm3  psa 
+    #    psb  pst  pnu  ph  
+    #    ptb  pca  pcx  pde
+    #    pct  cfa  cfx  cft  
+    # =======================
+    initVector = np.array([
+        0.000990214267170159,9.318719739708396e-05,4.542275518749989,10.385969331018355,
+        0.21539389927593255,6.062205518675469,0.00088947580412008,3.8368121167519265,
+        2.9935202002665857,0.0008366572847518542,0.0003665298336643424,41.510057191938614,
+        9.818065814677865,0.0004235577417342571,0.0009069921643147143,1.4152161322301913 ])
+    
+    er = calculate_error(initVector, rsE.tolist(), tsV, log=sys.stdout)
+    print('Calculated error: ', er)
+
+
---
+++ complex-model/scanner/optimizer.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+from scipy.optimize import minimize
+from scipy.optimize import dual_annealing
+import launcher_1m
+import numpy as np
+
+def MyCallback(xk, log, fval=None, ctx=None):
+    print('STEP DONE!')
+    print('STEP PARAMETERS: ' + ' '.join(map(str, xk)) )
+
+# =======================
+#    pna   pnx  pm3  psa 
+#    psb   pst  pnu  ph  
+#    ptb   pca  pcx  pde 
+#    smt   cfa  cfx  cft
+# =======================
+
+initVector = [ 
+    0.000990214267170159,9.318719739708396e-05,4.542275518749989,10.385969331018355,
+    0.21539389927593255,6.062205518675469,0.00088947580412008,3.8368121167519265,
+    2.9935202002665857,0.0008366572847518542,0.0003665298336643424,41.510057191938614,
+    9.818065814677865,0.0004235577417342571,0.0009069921643147143,1.4152161322301913 ]
+
+initBounds = ( (0,1e-3), (0,1e-3), (0,10), (6,20), 
+               (0,5), (1,30), (5e-5,1e-3), (0.1,4),
+               (0,10), (0,1e-3), (0,1e-3), (20,150), 
+               (3.,10.), (0,1e-2), (0,1e-3), (0,10.) )
+#               0      1        2       3      4       5        6       7     8      9
+equiRads =   [ 725.,   551.,   698.,   538.,  670.,   649.,    637.,   791., 785., 770. ]
+#               K   |  Hf   |  -N2  | Hf-N2 |  K   |  -M3  |  -M3-N2 |  K  | -C  | -C-N2
+targetVals = np.array([
+             [ 256.,   327.,   288.,   214.,   239.,  386.,    309.,   296., 358., 361. ],
+             [ None,  None,    None,   None,  1374.,  989.,    None,   None, None, None ]
+                  ])
+
+flog = open('logoptim.log', 'a')
+
+def cbck(x,f,s):
+    print(x)
+    print(f)
+    print(s)
+    
+res = dual_annealing(lambda x,y,z: launcher_1m.calculate_error(x,y,z, log=flog), \
+        x0=initVector, \
+        args=( equiRads, targetVals, ), \
+        bounds=initBounds,
+        initial_temp=100, \
+        visit=1.5,
+        callback=cbck \
+        )
+
+flog.close()
